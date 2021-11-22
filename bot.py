61877ce85dbc6cde08162aa2cd3c65b5f2c89320
Alex Merk <durango77@gmail.com>
2021-11-22 21:40:48 +0300

    Мой первый бот

---
+++ bot.py
@@ -0,0 +1,43 @@
+import logging
+import settings
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+logging.basicConfig(filename='bot.log', level=logging.INFO)
+
+PROXY = {'proxy_url': settings.PROXY_URL,
+         'urllib3_proxy_kwargs': {'username': settings.PROXY_USERNAME, 'password': settings.PROXY_PASSWORD}}
+
+
+def greet_user(update, context):
+    print("Вызван /start")
+    update.message.reply_text('Вечер в хату! Ты вызвал команду /start')
+
+
+def talk_to_me(update, context):
+    user_text = update.message.text
+    print(user_text)
+    update.message.reply_text(user_text)
+
+
+def main():
+
+    mybot = Updater(
+        settings.API_KEY, use_context=True, request_kwargs=PROXY)
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    logging.info('бот стартовал')
+
+    mybot.start_polling()
+
+    mybot.idle()
+
+
+def greet_user(update, context):
+    print("Вызван /start")
+
+    update.message.reply_text('Вечер в хату! Ты вызвал команду /start')
+
+
+if __name__ == "__main__":
+    main()
