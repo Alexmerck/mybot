a3e33bcb4e7c181f042aeb6121ef124e2ff97714
Alex Merk <durango77@gmail.com>
2021-11-27 16:00:08 +0300

    Интегрировал ephem для работы с одной планетой

--- bot.py
+++ bot.py
@@ -1,16 +1,28 @@
 import logging
 import settings
+import ephem
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from datetime import date
 
 logging.basicConfig(filename='bot.log', level=logging.INFO)
 
 PROXY = {'proxy_url': settings.PROXY_URL,
          'urllib3_proxy_kwargs': {'username': settings.PROXY_USERNAME, 'password': settings.PROXY_PASSWORD}}
 
+today = date.today()
 
-def greet_user(update, context):
-    print("Вызван /start")
-    update.message.reply_text('Вечер в хату! Ты вызвал команду /start')
+
+def planet(update, context):
+    print("Вызван /planet")
+    user_planet = update.message.text
+    if 'Mars' in user_planet:
+        mars = ephem.Mars(today)
+        constellation = ephem.constellation(mars)
+        print(constellation)
+        update.message.reply_text(
+            f'Планета находится в созвездии {constellation}')
+    else:
+        update.message.reply_text('Такой планеты нет')
 
 
 def talk_to_me(update, context):
@@ -25,6 +37,7 @@ def main():
         settings.API_KEY, use_context=True, request_kwargs=PROXY)
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", planet))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     logging.info('бот стартовал')
 
